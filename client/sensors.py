47d73d78fc034b5c24decb8c6acbe81eb6df249e
tin1254 <tin-1254@hotmail.com>
2020-04-06 19:16:53 +0200

    cleanup cameraManager, support retrieving data from multiple sensors

--- client/sensors.py
+++ client/sensors.py
@@ -10,48 +10,47 @@ try:
 except IndexError:
     pass
 import carla
-from carla import ColorConverter as cc
+# from carla import ColorConverter as cc
 
 
 class CameraManager:
-  def __init__(self, cameras, synchronous_mode=False):
-    self.synchronous_mode = synchronous_mode
-    self.cams = cameras
-    self.camsIdx = self.cams.keys()
-    self.surfaces = dict.fromkeys(self.camsIdx)
-    self.queues = []
+    def __init__(self, cameras, synchronous_mode=False):
+        self.synchronous_mode = synchronous_mode
+        self.cams = cameras
+        self.camera_indice = cameras.keys()
+        self.surfaces = dict.fromkeys(self.camera_indice)
+        self.queues = []
 
-    for c in self.cams.values():
-      self._make_queue(self.queues, c.listen)
-    
+        for c in self.cams.values():
+            self._make_queue(self.queues, c.listen)
 
-  def _make_queue(self,queues, register_event):
-          q = Queue()
-          register_event(q.put)
-          queues.append(q)
+    def _make_queue(self, queues, register_event):
+        q = Queue()
+        register_event(q.put)
+        queues.append(q)
 
-  def _retrieve_data(self,sensor_queue, frame_id, timeout):
-    while True:
-      data = sensor_queue.get(timeout=timeout)
-      if data.frame == frame_id:
+    def _retrieve_data(self, sensor_queue, frame_id, timeout):
+        while True:
+            data = sensor_queue.get(timeout=timeout)
+            if data.frame == frame_id:
+                return data
+
+    def tick(self, frame_id, timeout=2):
+        data = [self._retrieve_data(q, frame_id, timeout) for q in self.queues]
         return data
 
-  def tick(self, frame_id, timeout=2):
-    data = [self._retrieve_data(q, frame_id, timeout) for q in self.queues]
-    return data
+    def _process_image(self, image):
+        array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+        array = np.reshape(array, (image.height, image.width, 4))
+        array = array[:, :, :3]
+        array = array[:, :, ::-1]
+        return array
 
-  def _process_image(self, image):
-    array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-    array = np.reshape(array, (image.height, image.width, 4))
-    array = array[:, :, :3]
-    array = array[:, :, ::-1]
-    return array
+    def fetch_image(self, frame_id, agents_id=None):
+        rawImages = self.tick(frame_id)
+        idx = self.camera_indice if agents_id is None else agents_id
 
-  def fetch_image(self, frame_id, agents_id=None):
-    rawImages = self.tick(frame_id)
-    idx = self.camsIdx if agents_id is None else agents_id
-
-    for idx, rawImage in zip(idx, rawImages):
-      image = self._process_image(rawImage)
-      self.surfaces[idx] = pg.surfarray.make_surface(
-          image.swapaxes(0, 1))
+        for idx, rawImage in zip(idx, rawImages):
+            image = self._process_image(rawImage)
+            self.surfaces[idx] = pg.surfarray.make_surface(
+                image.swapaxes(0, 1))
